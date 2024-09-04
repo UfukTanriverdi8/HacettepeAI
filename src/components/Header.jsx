ea8c37402498e68667b993942423bdf10bcd3dc8
Ufuk Tanrıverdi <ufuktanriverdi1@gmail.com>
2024-09-04 18:52:35 +0300

    lang switch styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,12 +9,31 @@ const Header = ({language, handleLanguageChange}) => {
         </h1>
         
         <div className="w-1/3 flex justify-end items-center mr-3 text-base">
-        <div className="relative flex items-center cursor-pointer w-20 h-10 bg-black rounded-full" onClick={handleLanguageChange}>
-          <div className={`absolute w-10 h-10 bg-secondary rounded-full transition-transform duration-300 ${language === 'TR' ? 'translate-x-10' : 'translate-x-0'}`}></div>
-          <span className={`absolute left-2 text-tertiary ${language === 'EN' ? 'font-bold' : 'text-opacity-50'}`}>EN</span>
-          <span className={`absolute right-2 text-tertiary ${language === 'TR' ? 'font-bold' : 'text-opacity-50'}`}>TR</span>
+            <div 
+            className="relative flex items-center cursor-pointer w-14 sm:w-16 h-7 sm:h-8 bg-black rounded-full" 
+            onClick={handleLanguageChange}
+            >
+            <div 
+                className={`absolute w-7 sm:w-8 h-7 sm:h-8 bg-secondary rounded-full transition-transform duration-300 ${
+                language === 'TR' ? 'translate-x-7 sm:translate-x-8' : 'translate-x-0'
+                }`}
+            ></div>
+            <span 
+                className={`absolute left-1 sm:left-2 text-tertiary text-xs sm:text-sm ${
+                language === 'EN' ? 'font-bold' : 'text-opacity-50'
+                }`}
+            >
+                EN
+            </span>
+            <span 
+                className={`absolute right-1 sm:right-2 text-tertiary text-xs sm:text-sm ${
+                language === 'TR' ? 'font-bold' : 'text-opacity-50'
+                }`}
+            >
+                TR
+            </span>
+            </div>
         </div>
-      </div>
       </div>
     )
   }
