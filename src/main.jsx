864bd9e850a587b3abd2fec96413dba8aa69a23c
Ufuk Tanrıverdi <ufuktanriverdi1@gmail.com>
2026-02-05 23:22:11 +0300

    vercel analytics removed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./index.css"
-import { Analytics } from '@vercel/analytics/react';
 
 
 createRoot(document.getElementById('root')).render(
